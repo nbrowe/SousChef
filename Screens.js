0efbe65893d1026b6f1ae17eeda6b7856071e676
nbrowe <naterogen@outlook.com>
2021-06-07 01:10:22 -0400

    initial code copying, 4:41

--- Screens.js
+++ Screens.js
@@ -35,3 +35,28 @@ export const Details = () => (
 // export const Search
 
 // search2
+
+export const Splash = () => (
+  <ScreenContainer>
+    <Text>Loading/-\+</Text>
+  </ScreenContainer>
+);
+
+export const SignIn = ({ navigation}) => {
+  return (
+    <ScreenContainer>
+      <Text>Sign IN SCREEN</Text>
+      <Button title="Sign OON" onPress={() => alert('todo!')} />
+      <Button title='Create ACCount' onPress={() => alert('todo!')} />
+    </ScreenContainer>
+  );
+};
+
+export const CreateAccount = () => {
+  return (
+    <ScreenContainer>
+      <Text>Create Account Screeeeen</Text>
+      <Button title='Sign Upp' onPress={() => alert('todo!')} />
+    </ScreenContainer>
+  );
+};
