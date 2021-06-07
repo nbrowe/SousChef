38a70ac08e65491f815d77f1889aa52ec290f464
nbrowe <naterogen@outlook.com>
2021-06-07 16:17:41 -0400

    finals edits before submissions of hw1

--- Screens.js
+++ Screens.js
@@ -1,22 +1,47 @@
-// This code was grabbed directly from React Native School
-// using the tutorial available at https://www.youtube.com/watch?v=nQVCkqvU1uE
-// Source repo: https://github.com/ReactNativeSchool/getting-started-react-navigation-v5
+// Nathaniel Rowe 2021
+// nbrowe@brandeis.edu
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
-
-import { AuthContext } from "./context";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  Image,
+  TextInput
+} from "react-native";
 
 const styles = StyleSheet.create({
   container: {
+    alignItems: "center",
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center"
+    justifyContent: "center"
+  },
+  mainTitle: {
+    color: 'red',
   },
   button: {
-    paddingHorizontal: 20,
-    paddingVertical: 10,
+    borderRadius: 5,
     marginVertical: 10,
-    borderRadius: 5
+    paddingHorizontal: 20,
+    paddingVertical: 10
+  },
+  horizContainer: {
+    alignItems: "stretch",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    padding: 10
+  },
+  bigPic: {
+    height: 200,
+    resizeMode: 'contain',
+    width: '100%'
+  },
+  input: {
+    borderWidth: 1,
+    height: 40,
+    margin: 12,
+    padding: 10
   }
 });
 
@@ -24,29 +49,64 @@ const ScreenContainer = ({ children }) => (
   <View style={styles.container}>{children}</View>
 );
 
-export const Home = ({ navigation }) => (
-  <ScreenContainer>
-    <Text>Master List Screen</Text>
-    <Button
-      title="React Native by Nathaniel"
-      onPress={() =>
-        navigation.push("Details", { name: "React Native by Nathaniel" })
-      }
-    />
-    <Button
-      title="React Native School"
-      onPress={() =>
-        navigation.push("Details", { name: "React Native School" })
-      }
-    />
-    <Button title="Drawer" onPress={() => navigation.toggleDrawer()} />
-  </ScreenContainer>
-);
+export const Home = ({ navigation }) => {
+  const [number, onChangeNumber] = React.useState(12);
+
+  return (
+    <ScreenContainer>
+      <Text
+        id='homeHeader'
+        style={[styles.mainTitle, {fontSize: parseInt(number) || 12 }]}  // Using array op to combine
+      >
+        Lorem ipsum, change my font size
+      </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        placeholder='type font size (Default: 12)' />
+      <Image
+        source={require('./assets/images/one.png')}
+      />
+      <View style={styles.horizContainer}>
+        <Button
+          style={styles.button}
+          title="Inspect Image 1"
+          onPress={() =>
+            navigation.push("Details", {
+              name: "Image 1",
+              image: require('./assets/images/one.png')
+            })
+          }
+        />
+        <Button
+          style={styles.button}
+          title="Inspect Image 2"
+          onPress={() =>
+            navigation.push("Details", {
+              name: "Image 2",
+              image: require('./assets/images/two.png')
+            })
+          }
+        />
+      </View>
+      {/* <Button
+      style={styles.button}
+      title="Open Drawer"
+      onPress={() => navigation.toggleDrawer()}
+    /> */}
+    </ScreenContainer>
+  );
+}
 
 export const Details = ({ route }) => (
   <ScreenContainer>
     <Text>Details Screen</Text>
     {route.params.name && <Text>{route.params.name}</Text>}
+    {route.params.image && <Image
+      style={styles.bigPic}
+      source={route.params.image}
+    />}
+
   </ScreenContainer>
 );
 
@@ -55,11 +115,11 @@ export const Search = ({ navigation }) => (
     <Text>Search Screen</Text>
     <Button title="Search 2" onPress={() => navigation.push("Search2")} />
     <Button
-      title="React Native School"
+      title="DummyNavigator"
       onPress={() => {
         navigation.navigate("Home", {
           screen: "Details",
-          params: { name: "React Native School" }
+          params: { name: "screen not implemented" }
         });
       }}
     />
@@ -68,50 +128,12 @@ export const Search = ({ navigation }) => (
 
 export const Search2 = () => (
   <ScreenContainer>
-    <Text>Search2 Screen</Text>
+    <Text>Search2 Screen for later implementation</Text>
   </ScreenContainer>
 );
 
-export const Profile = ({ navigation }) => {
-  const { signOut } = React.useContext(AuthContext);
-
-  return (
-    <ScreenContainer>
-      <Text>Profile Screen</Text>
-      <Button title="Drawer" onPress={() => navigation.toggleDrawer()} />
-      <Button title="Sign Out" onPress={() => signOut()} />
-    </ScreenContainer>
-  );
-};
-
-export const Splash = () => (
-  <ScreenContainer>
-    <Text>Loading...</Text>
-  </ScreenContainer>
-);
-
-export const SignIn = ({ navigation }) => {
-  const { signIn } = React.useContext(AuthContext);
-
-  return (
-    <ScreenContainer>
-      <Text>Sign In Screen</Text>
-      <Button title="Sign In" onPress={() => signIn()} />
-      <Button
-        title="Create Account"
-        onPress={() => navigation.push("CreateAccount")}
-      />
-    </ScreenContainer>
-  );
-};
-
-export const CreateAccount = () => {
-  const { signUp } = React.useContext(AuthContext);
-
-  return (
-    <ScreenContainer>
-      <Text>Create Account Screen</Text>
-      <Button title="Sign Up" onPress={() => signUp()} />
-    </ScreenContainer>
-  );
-};
+// export const Splash = () => (
+//   <ScreenContainer>
+//     <Text>Loading</Text>
+//   </ScreenContainer>
+// );
