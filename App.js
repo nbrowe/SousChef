bd173881ea0080050be8eb4a8397967692f0825b
nbrowe <naterogen@outlook.com>
2021-06-07 02:13:54 -0400

    timestamp 15:02

--- App.js
+++ App.js
@@ -1,16 +1,48 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
-import { SignIn, CreateAccount } from './Screens';
+import { SignIn, CreateAccount, Search, Home, Details, Search2 } from './Screens';
 
-const AuthStack = createStackNavigator();
+// const AuthStack = createStackNavigator();
+const Tabs = createBottomTabNavigator();
+const HomeStack = createStackNavigator();
+const SearchStack = createStackNavigator();
+
+const HomeStackScreen = () => (
+  <HomeStack.Navigator>
+    <HomeStack.Screen name='Home' component={Home} />
+    <HomeStack.Screen name='Details' component={Details} options={({ route }) => ({
+      title: route.params.name
+    })} />
+  </HomeStack.Navigator>
+);
+
+const SearchStackScreen = () => (
+  <SearchStack.Navigator>
+    <SearchStack.Screen name='Search' component={Search} />
+    <SearchStack.Screen name='Search2' component={Search2} />
+  </SearchStack.Navigator>
+);
 
 export default () => (
   <NavigationContainer>
-    <AuthStack.Navigator>
-      <AuthStack.Screen name='SignIn' component={SignIn} />
-      <AuthStack.Screen name='CreateAccount' component={CreateAccount} />
-    </AuthStack.Navigator>
-  </NavigationContainer>
+    <Tabs.Navigator>
+      <Tabs.Screen name='Home' component={HomeStackScreen} />
+      <Tabs.Screen name='Profile' component={SearchStackScreen} />
+    </Tabs.Navigator>
+    {/* <AuthStack.Navigator>
+      <AuthStack.Screen
+        name='SignIn'
+        component={SignIn}
+        options={{ title: 'Sign In' }}
+      />
+      <AuthStack.Screen
+        name='CreateAccount'
+        component={CreateAccount}
+        options={{ title: 'Create Account' }}
+      />
+  </AuthStack.Navigator> */}
+  </NavigationContainer >
 )
--- Screens.js
+++ Screens.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Button } from "react-native";
+
+// import { AuthContext } from "./context";
 
 const styles = StyleSheet.create({
-  continer: {
+  container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center"
   },
   button: {
     paddingHorizontal: 20,
@@ -19,44 +21,94 @@ const ScreenContainer = ({ children }) => (
   <View style={styles.container}>{children}</View>
 );
 
-// export const Home = () => (
-//   <ScreenContainer>
-//     <Text>Master List Screen</Text>
-//     <Button title='React Native by Example'
-//     //finish this
-// )
-
-export const Details = () => (
+export const Home = ({ navigation }) => (
   <ScreenContainer>
-    <Text>Details Screen</Text>
+    <Text>Master List Screen</Text>
+    <Button
+      title="React Native by Nathaniel"
+      onPress={() =>
+        navigation.push("Details", { name: "React Native by Nathaniel" })
+      }
+    />
+    <Button
+      title="React Native School"
+      onPress={() =>
+        navigation.push("Details", { name: "React Native School" })
+      }
+    />
+    <Button title="Drawer" onPress={() => navigation.toggleDrawer()} />
   </ScreenContainer>
 );
 
-// export const Search
+export const Details = ({ route }) => (
+  <ScreenContainer>
+    <Text>Details Screen</Text>
+    {route.params.name && <Text>{route.params.name}</Text>}
+  </ScreenContainer>
+);
 
-// search2
+export const Search = ({ navigation }) => (
+  <ScreenContainer>
+    <Text>Search Screen</Text>
+    <Button title="Search 2" onPress={() => navigation.push("Search2")} />
+    <Button
+      title="React Native School"
+      onPress={() => {
+        navigation.navigate("Home", {
+          screen: "Details",
+          params: { name: "React Native School" }
+        });
+      }}
+    />
+  </ScreenContainer>
+);
+
+export const Search2 = () => (
+  <ScreenContainer>
+    <Text>Search2 Screen</Text>
+  </ScreenContainer>
+);
+
+export const Profile = ({ navigation }) => {
+  // const { signOut } = React.useContext(AuthContext);
+
+  return (
+    <ScreenContainer>
+      <Text>Profile Screen</Text>
+      <Button title="Drawer" onPress={() => navigation.toggleDrawer()} />
+      <Button title="Sign Out" onPress={() => signOut()} />
+    </ScreenContainer>
+  );
+};
 
 export const Splash = () => (
   <ScreenContainer>
-    <Text>Loading/-\+</Text>
+    <Text>Loading...</Text>
   </ScreenContainer>
 );
 
-export const SignIn = ({ navigation}) => {
+export const SignIn = ({ navigation }) => {
+  // const { signIn } = React.useContext(AuthContext);
+
   return (
     <ScreenContainer>
-      <Text>Sign IN SCREEN</Text>
-      <Button title="Sign OON" onPress={() => alert('todo!')} />
-      <Button title='Create ACCount' onPress={() => alert('todo!')} />
+      <Text>Sign In Screen</Text>
+      <Button title="Sign In" onPress={() => signIn()} />
+      <Button
+        title="Create Account"
+        onPress={() => navigation.push("CreateAccount")}
+      />
     </ScreenContainer>
   );
 };
 
 export const CreateAccount = () => {
+  // const { signUp } = React.useContext(AuthContext);
+
   return (
     <ScreenContainer>
-      <Text>Create Account Screeeeen</Text>
-      <Button title='Sign Upp' onPress={() => alert('todo!')} />
+      <Text>Create Account Screen</Text>
+      <Button title="Sign Up" onPress={() => signUp()} />
     </ScreenContainer>
   );
 };
